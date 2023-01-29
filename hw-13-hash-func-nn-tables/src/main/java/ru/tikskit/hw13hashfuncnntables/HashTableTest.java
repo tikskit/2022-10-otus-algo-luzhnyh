a9c84795bf6348ff8b74ba8ed5bcfcbf7bfa83b8
tikskit <tikskit@gmail.com>
2023-01-29 22:14:51 +0700

    Домашка по №13 "Хэш-функции и хэш-таблицы"

--- hw-13-hash-func-nn-tables/src/main/java/ru/tikskit/hw13hashfuncnntables/HashTableTest.java
+++ hw-13-hash-func-nn-tables/src/main/java/ru/tikskit/hw13hashfuncnntables/HashTableTest.java
@@ -10,6 +10,7 @@ public class HashTableTest {
         testPopulate();
         testPopulateWithDoubles();
         testRemove();
+        testRemoveNotExistingValue();
     }
 
     private static void testPopulate() {
@@ -87,12 +88,41 @@ public class HashTableTest {
         data.keySet().forEach(
                 k -> hashTable.put(k, data.get(k))
         );
+
         data.keySet().forEach(
                 k -> {
+                    int prevSize = hashTable.getSize();
                     hashTable.remove(k);
                     Integer value = hashTable.get(k);
                     if (value != null) {
                         throw new IllegalStateException(String.format("Значение ключа {} не удалено", k));
+                    }
+
+                    if (hashTable.getSize() != prevSize - 1) {
+                        throw new IllegalStateException("Неправильно уменьшается размер таблицы");
+                    }
+                }
+        );
+    }
+
+    private static void testRemoveNotExistingValue() {
+        Map<Integer, Integer> data = IntStream.range(0, 20)
+                .boxed()
+                .collect(toMap(Integer::intValue, Integer::intValue));
+        HashTable<Integer, Integer> hashTable = new HashTable<>();
+        data.keySet().forEach(
+                k -> hashTable.put(k, data.get(k))
+        );
+        int prevSize = hashTable.getSize();
+        hashTable.remove(10002);
+        if (hashTable.getSize() != prevSize) {
+            throw new IllegalStateException("Было изменен размер таблицы");
+        }
+        // все значения остались на месте
+        data.keySet().forEach(
+                k -> {
+                    if (hashTable.get(k) == null) {
+                        throw new IllegalStateException(String.format("Не найдено значение {}", k));
                     }
                 }
         );
