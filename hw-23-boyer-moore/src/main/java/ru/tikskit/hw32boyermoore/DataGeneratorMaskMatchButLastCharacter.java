7a586f5b10fce770c0d7aa2f9ec9f145eb5ba4ec
tikskit <tikskit@gmail.com>
2023-03-11 11:56:02 +0700

    23 Алгоритм Бойера-Мура

--- hw-23-boyer-moore/src/main/java/ru/tikskit/hw32boyermoore/DataGeneratorMaskMatchButLastCharacter.java
+++ hw-23-boyer-moore/src/main/java/ru/tikskit/hw32boyermoore/DataGeneratorMaskMatchButLastCharacter.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * Генератор тестовых данных, в которых маска совпадает, кроме последнего символа
  */
 public class DataGeneratorMaskMatchButLastCharacter implements DataGenerator {
-    private static final int TEXT_LENGTH = 10000000;
+    private static final int TEXT_LENGTH = 100000;
     private static final int MASK_LENGTH = 1000;
     @Override
     public Data gen() {
@@ -15,7 +15,7 @@ public class DataGeneratorMaskMatchButLastCharacter implements DataGenerator {
         Arrays.fill(textArr, 'A');
 
         char[] maskArr = new char[MASK_LENGTH];
-        Arrays.fill(textArr, 'A');
+        Arrays.fill(maskArr, 'A');
         maskArr[MASK_LENGTH - 1] = 'B';
 
         return new Data(new String(textArr), new String(maskArr), "Mask match but last character");
--- hw-23-boyer-moore/src/main/java/ru/tikskit/hw32boyermoore/DataProviderImpl.java
+++ hw-23-boyer-moore/src/main/java/ru/tikskit/hw32boyermoore/DataProviderImpl.java
@@ -3,11 +3,12 @@ package ru.tikskit.hw32boyermoore;
 public class DataProviderImpl implements DataProvider {
     @Override
     public Data[] getData() {
-        Data[] res = new Data[3];
+        Data[] res = new Data[4];
 
         res[0] = (new DataGeneratorRandomBlocks()).gen();
         res[1] = (new DataGeneratorMaskNeverMatch()).gen();
         res[2] = (new DataGeneratorMaskMatchButLastCharacter()).gen();
+        res[3] = (new DataGeneratorMaskMatchButFirstCharacter()).gen();
         return res;
     }
 }
