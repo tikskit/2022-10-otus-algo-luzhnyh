994c231efd5d81fb17ab4003d5912751642b732d
tikskit <tikskit@gmail.com>
2023-03-22 10:52:33 +0700

    23 алгоритм бойера-мура

--- hw-23-boyer-moore/src/main/java/ru/tikskit/hw32boyermoore/DataProviderImpl.java
+++ hw-23-boyer-moore/src/main/java/ru/tikskit/hw32boyermoore/DataProviderImpl.java
@@ -3,12 +3,14 @@ package ru.tikskit.hw32boyermoore;
 public class DataProviderImpl implements DataProvider {
     @Override
     public Data[] getData() {
-        Data[] res = new Data[4];
+        Data[] res = new Data[6];
 
         res[0] = (new DataGeneratorRandomBlocks()).gen();
         res[1] = (new DataGeneratorMaskNeverMatch()).gen();
         res[2] = (new DataGeneratorMaskMatchButLastCharacter()).gen();
         res[3] = (new DataGeneratorMaskMatchButFirstCharacter()).gen();
+        res[4] = (new DataGeneratorTailMatch()).gen();
+        res[5] = (new DataGeneratorTailMatchRandom()).gen();
         return res;
     }
 }
